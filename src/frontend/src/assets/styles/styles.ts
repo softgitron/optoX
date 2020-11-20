110a20a985829dd857b242b4dbdadd1bd6a6a296
Juho Kontiainen <juho.kontiainen@visma.com>
2020-11-20 20:02:57 +0200

    Changed Autocomplete Styles in WebKit Browsers

--- src/frontend/src/assets/styles/styles.ts
+++ src/frontend/src/assets/styles/styles.ts
@@ -2,9 +2,9 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const useOutlinedInputStyles = makeStyles((theme) => ({
   root: {
-    "& $notchedOutline": {
+    /*     "& $notchedOutline": {
       backgroundColor: "#65656517",
-    },
+    }, */
     "&:hover $notchedOutline": {
       borderColor: "#037a6f",
     },
